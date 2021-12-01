71f78d96d128543c1934d4aae447962de5833b64
Nate Dunn <hello@natedunn.net>
2021-11-30 22:50:45 -0600

    Test functional component

--- components/Test.tsx
+++ components/Test.tsx
@@ -1,18 +1,28 @@
 // Import React
 import React from "react";
 
-// Create React Component
-export default class Test extends React.Component<Props> {
-  render() {
-    return (
-      <div className={this.props.className}>
-        Hellooooooo: {this.props.name ?? "Unnamed"}
-      </div>
-    );
-  }
-}
-
 interface Props {
   name: string;
   className: string;
 }
+
+// Create React Component
+// class Test extends React.Component<Props> {
+//   render() {
+//     return (
+//       <div className={this.props.className}>
+//         Hellooooooo: {this.props.name ?? "Unnamed"}
+//       </div>
+//     );
+//   }
+// }
+
+const Test = (props: Props) => {
+  return (
+    <div className={props.className}>
+      Hello there: {props.name ?? "Unnamed"}
+    </div>
+  );
+};
+
+export default Test;
